52acd72501d5236f538c4d851def91c34b2de56e
Shao Zhang <szhang2@ualberta.ca>
2023-02-18 00:28:31 -0700

    updated state to render new bosses killed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,18 +7,34 @@ import queryFights from "../query"
 
 function App() {
     // const [logCode, setLogCode] = useState("");
-    const [bossesKilled, setBossesKilled] = useState(["boss1"])
+
+    const [bossesKilled, setBossesKilled] = useState([])
+
 
     async function parseLogLink(link) {
         const splitStr = link.split("/");
         await getAccessToken();
-        let fights = await queryFights(splitStr[splitStr.length - 2]);
+        // gets query of bosses killed
+        let bossKill = await queryFights(splitStr[splitStr.length - 2]);
+        setBossesKilled(()=>{
+            console.log("hello");
+            let tempArray = []
+            for (let boss of bossKill){
+                tempArray.push(boss.name)
+            }
+            return tempArray
+        })
+        console.log()
     }
+    
 
     return (
         <div>
             <Header />
             <SearchBar getLogCode={parseLogLink}/>
+            {bossesKilled.map((bosses) =>{
+                return <p key={bosses}>{bosses}</p>
+            })}
         </div>
     )
 }
