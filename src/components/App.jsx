89530eeb0fa540fcdeb9f5b6201f405b271b1c3c
Shao Zhang <szhang2@ualberta.ca>
2023-02-18 14:49:29 -0700

    render boss name onto MUI Card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,28 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import SearchBar from "./SearchBar";
 import Header from "./Header";
 import getAccessToken from "../tokenService";
-import queryFights from "../query"
+import queryFights from "../query";
+import DisplayCard from "../components/Card";
 
 
 function App() {
     // const [logCode, setLogCode] = useState("");
 
-    const [bossesKilled, setBossesKilled] = useState([])
+    // const [bossesLeft, setBossesLeft] = useState(["Flame Leviathan",
+    //     "Ignis the Furnace Master", "Razorscale", "XT-002 Deconstructor",
+    //     "The Assembly of Iron", "Kologarn", "Auriaya", "Hodir", "Thorim",
+    //     "Freya", "Mimiron", "General Vezax", "Yogg-Saron"])
+
+    // const [bossesKilled, setBossesKilled] = useState([])
+
+    const [raidState, setRaidState] = useState({
+        bossesKilled: [],
+        bossesLeft: ["Flame Leviathan",
+            "Ignis the Furnace Master", "Razorscale", "XT-002 Deconstructor",
+            "The Assembly of Iron", "Kologarn", "Auriaya", "Hodir", "Thorim",
+            "Freya", "Mimiron", "General Vezax", "Yogg-Saron"]
+    })
 
 
     async function parseLogLink(link) {
@@ -16,25 +30,51 @@ function App() {
         await getAccessToken();
         // gets query of bosses killed
         let bossKill = await queryFights(splitStr[splitStr.length - 2]);
-        setBossesKilled(()=>{
-            console.log("hello");
-            let tempArray = []
-            for (let boss of bossKill){
-                tempArray.push(boss.name)
+        // setBossesKilled(() => {
+        //     let tempArray = [];
+        //     for (let boss of bossKill) {
+        //         tempArray.push(boss.name);
+        //     }
+        //     console.log("setBOssesKilled comeplete")
+        //     return tempArray;
+        // })
+        setRaidState((prevState) => {
+            // tempArray contains bosses killed from log
+            let tempArray = [];
+            for (let boss of bossKill) {
+                tempArray.push(boss.name);
             }
-            return tempArray
+            return ({
+                bossesKilled: tempArray,
+                bossesLeft: prevState.bossesLeft.filter(name => !tempArray.includes(name))
+            })
+
         })
-        console.log()
+        // setBossesLeft((prevArray) => {
+        //     let newArray = prevArray.filter(name => !bossesKilled.includes(name))
+        //     console.log(newArray)
+        //     return newArray
+        // })
     }
-    
+
 
     return (
         <div>
             <Header />
-            <SearchBar getLogCode={parseLogLink}/>
-            {bossesKilled.map((bosses) =>{
-                return <p key={bosses}>{bosses}</p>
-            })}
+            <SearchBar getLogCode={parseLogLink} />
+            <h2>Upcoming Bosses</h2>
+            <div className="card-container">
+                {raidState.bossesLeft?.map((boss) => {
+                    return <DisplayCard key={boss} name={boss} class="boss-card" />
+                })}
+            </div>
+            <h2>Bosses Killed</h2>
+            <div className="card-container">
+                {raidState.bossesKilled?.map((boss) => {
+                    return <DisplayCard key={boss} name={boss} class="boss-card" />
+                })}
+            </div>
+
         </div>
     )
 }
